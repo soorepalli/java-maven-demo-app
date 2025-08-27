d4b8e1d5c61748e61349a0e275ea3efb97e51a11
Raja Ramesh SP <46845645+soorepalli@users.noreply.github.com>
2025-08-27 07:13:30 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Deploy Jar in Docker";
+    private static final String MESSAGE = "Java application running under Docker Container";
 
     public App() {}
 
